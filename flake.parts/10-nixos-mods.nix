0cb6d4b16cb6fb13d26f334c299c0418f953a22c
adanoelle <adanoelleyoung@gmail.com>
2025-06-26 10:01:17 -0400

    fix: Rename bootloader to be consistent

--- flake.parts/10-nixos-mods.nix
+++ flake.parts/10-nixos-mods.nix
@@ -1,7 +1,7 @@
 { self, ... }: {
   flake.nixosModules = {
     audio     = import ../nix/modules/audio.nix;
-    boot      = import ../nix/modules/bootloader.nix;
+    boot      = import ../nix/modules/boot.nix;
     graphics  = import ../nix/modules/graphics.nix;
     rust-dev  = import ../nix/modules/devtools/rust.nix;
     users     = import ../nix/modules/users.nix;
