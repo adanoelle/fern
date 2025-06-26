3c8084ed35c2b4ff7a8b903848109fdf8a87ce5f
adanoelle <adanoelleyoung@gmail.com>
2025-06-26 13:15:50 -0400

    feat: Add hosts flake part for fern

--- flake.nix
+++ flake.nix
@@ -1,10 +1,18 @@
 {
   inputs = {
-    nixpkgs.url        = "github:NixOS/nixpkgs/nixos-23.11";
-    flake-parts.url    = "github:hercules-ci/flake-parts";
-    flake-utils.url    = "github:numtide/flake-utils";
-    rust-overlay.url   = "github:oxalica/rust-overlay";
-    home-manager.url   = "github:nix-community/home-manager/release-23.11";
+    nixpkgs.url       = "github:NixOS/nixpkgs/nixos-23.11";
+    flake-parts.url   = "github:hercules-ci/flake-parts";
+    flake-utils.url   = "github:numtide/flake-utils";
+
+    rust-overlay = {
+      url   = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
+    home-manager = {
+      url   = "github:nix-community/home-manager/release-23.11";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = inputs@{ flake-parts, ... }:
@@ -13,6 +21,7 @@
         ./flake.parts/00-core.nix
         ./flake.parts/10-nixos-mods.nix
         ./flake.parts/20-home-mods.nix
+        ./flake.parts/30-hosts.nix
       ];
     };
 }
