ca57a1f2ad6eee645e4e9c5f843a52cf1a12f663
adanoelle <adanoelleyoung@gmail.com>
2025-06-26 13:25:21 -0400

    fix: Typo in path

--- flake.parts/10-nixos-mods.nix
+++ flake.parts/10-nixos-mods.nix
@@ -2,7 +2,7 @@
   flake.nixosModules = {
     audio     = import ../nix/modules/audio.nix;
     boot      = import ../nix/modules/boot.nix;
-    core      = import ../nix/modules.core.nix;
+    core      = import ../nix/modules/core.nix;
     graphics  = import ../nix/modules/graphics.nix;
     rust-dev  = import ../nix/modules/devtools/rust.nix;
     users     = import ../nix/modules/users.nix;
