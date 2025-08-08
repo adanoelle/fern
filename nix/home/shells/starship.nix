ffaef2ff6c86a75772c4f39d3a85c2f1075f75de
adanoelle <adanoelleyoung@gmail.com>
2025-08-08 14:03:19 +0000

    feat: Update config for starship

--- nix/home/shells/starship.nix
+++ nix/home/shells/starship.nix
@@ -1,12 +1,26 @@
-{ pkgs, ... }:
 {
+  config,
+  pkgs,
+  ...
+}: {
+  home.packages = with pkgs; [starship];
+
   programs.starship = {
     enable = true;
     settings = {
-      add_newline = false;
-      # prompt style tweaks
-      format = "$directory$character";
+      add_newline = true;
+      line_break.disabled = false;
+      character = {
+        success_symbol = "[➜](bold green)";
+      };
+      os = {
+        format = "$symbol";
+        style = "bold blue";
+        disabled = false;
+      };
+      os.symbols = {
+        NixOS = "❄️ ";
+      };
     };
   };
 }
-
